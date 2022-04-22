cfcf06b87706eea6b5045ec2c95fba9b9fc80077
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-22 23:09:07 +0300

    Added dependency treebanks annotated by Starlang.

--- DependencyParser/Universal/UniversalDependencyTreeBankCorpus.cs
+++ DependencyParser/Universal/UniversalDependencyTreeBankCorpus.cs
@@ -1,6 +1,4 @@
-using System;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace DependencyParser.Universal
 {
@@ -8,73 +6,22 @@ namespace DependencyParser.Universal
     {
         public UniversalDependencyTreeBankCorpus(string fileName)
         {
-            UniversalDependencyTreeBankSentence sentence = null;
-            UniversalDependencyRelation relation;
             var assembly = typeof(UniversalDependencyTreeBankCorpus).Assembly;
             var stream = assembly.GetManifestResourceStream("DependencyParser." + fileName);
             var streamReader = new StreamReader(stream);
             var line = streamReader.ReadLine();
+            var sentence = "";
             while (line != null)
             {
                 if (line.Length == 0)
                 {
-                    AddSentence(sentence);
-                    sentence = null;
+                    AddSentence(new UniversalDependencyTreeBankSentence(sentence));
+                    sentence = "";
                 }
                 else
                 {
-                    if (line.StartsWith("#"))
-                    {
-                        if (sentence == null)
-                        {
-                            sentence = new UniversalDependencyTreeBankSentence();
-                        }
-
-                        sentence.AddComment(line.Trim());
-                    }
-                    else
-                    {
-                        var items = line.Split('\t');
-                        if (items.Length != 10)
-                        {
-                            Console.WriteLine("Line does not contain 10 items ->" + line);
-                        }
-                        else
-                        {
-                            var id = items[0];
-                            if (new Regex("^\\d+$").IsMatch(id))
-                            {
-                                var surfaceForm = items[1];
-                                var lemma = items[2];
-                                var upos =
-                                    UniversalDependencyRelation.GetDependencyPosType(items[3]);
-
-                                var xpos = items[4];
-                                var features =
-                                    new UniversalDependencyTreeBankFeatures(items[5]);
-                                if (items[6] != "_")
-                                {
-                                    var to = int.Parse(items[6]);
-                                    var dependencyType = items[7].ToUpper();
-
-                                    relation = new UniversalDependencyRelation(to, dependencyType);
-                                }
-                                else
-                                {
-                                    relation = null;
-                                }
-
-                                var deps = items[8];
-                                var misc = items[9];
-                                var word = new UniversalDependencyTreeBankWord(
-                                    int.Parse(id), surfaceForm,
-                                    lemma, upos, xpos, features, relation, deps, misc);
-                                sentence.AddWord(word);
-                            }
-                        }
-                    }
+                    sentence += line + "\n";
                 }
-
                 line = streamReader.ReadLine();
             }
         }
--- DependencyParser/Universal/UniversalDependencyTreeBankSentence.cs
+++ DependencyParser/Universal/UniversalDependencyTreeBankSentence.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Corpus;
 
 namespace DependencyParser.Universal
@@ -10,6 +12,65 @@ namespace DependencyParser.Universal
         public UniversalDependencyTreeBankSentence()
         {
             comments = new List<string>();
+        }
+
+        public UniversalDependencyTreeBankSentence(string sentence)
+        {
+            UniversalDependencyRelation relation;
+            comments = new List<string>();
+            var lines = sentence.Split("\n");
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    continue;
+                }
+                if (line.StartsWith("#"))
+                {
+                    AddComment(line.Trim());
+                }
+                else
+                {
+                    var items = line.Split('\t');
+                    if (items.Length != 10)
+                    {
+                        Console.WriteLine("Line does not contain 10 items ->" + line);
+                    }
+                    else
+                    {
+                        var id = items[0];
+                        if (new Regex("^\\d+$").IsMatch(id))
+                        {
+                            var surfaceForm = items[1];
+                            var lemma = items[2];
+                            var upos =
+                                UniversalDependencyRelation.GetDependencyPosType(items[3]);
+
+                            var xpos = items[4];
+                            var features =
+                                new UniversalDependencyTreeBankFeatures(items[5]);
+                            if (items[6] != "_")
+                            {
+                                var to = int.Parse(items[6]);
+                                var dependencyType = items[7].ToUpper();
+
+                                relation = new UniversalDependencyRelation(to, dependencyType);
+                            }
+                            else
+                            {
+                                relation = null;
+                            }
+
+                            var deps = items[8];
+                            var misc = items[9];
+                            var word = new UniversalDependencyTreeBankWord(
+                                int.Parse(id), surfaceForm,
+                                lemma, upos, xpos, features, relation, deps, misc);
+                            AddWord(word);
+                        }
+                    }
+                }
+            }
         }
 
         public void AddComment(string comment)
